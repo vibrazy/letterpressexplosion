202f7fc320b1a1a013c1b879251e1526bee249ed
Daniel Tavares <collinsd@Daniels-MacBook-Pro.local>
2013-04-04 09:54:42 +0100

    Cleaning Up

--- LetterPressExplosion/LetterPressExplosion/DTVViewController.m
+++ LetterPressExplosion/LetterPressExplosion/DTVViewController.m
@@ -20,24 +20,14 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self.view setBackgroundColor:[UIColor blackColor]];
+    
+    [self createLetterPressImages];
+    
     __unsafe_unretained typeof(self) weakSelf = self;
 	// Do any additional setup after loading the view, typically from a nib.
     [self.view setTappedGestureWithBlock:^{
-        //  [weakSelf createLabel1];
-        //[weakSelf createLabel2];
         [weakSelf createLetterPressImages];
     }];
-    
-    UIView *bob = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
-    [bob setBackgroundColor:[UIColor blueColor]];
-    [self.view addSubview:bob];
-    
-    __unsafe_unretained typeof(bob) bobSelf = bob;
-    [bob setTappedGestureWithBlock:^{
-        [bobSelf explode];
-        [bobSelf setTappedGestureWithBlock:nil];
-    }];
-
 }
 -(void)createLetterPressImages
 {
